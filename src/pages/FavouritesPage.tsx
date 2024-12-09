971fc6d28401cfa0d9d578bcf3cfebee3450caca
Timur <bestekst0@gmail.com>
2024-12-09 22:01:33 +0300

    update FavouritesPage.tsx

--- src/pages/FavouritesPage.tsx
+++ src/pages/FavouritesPage.tsx
@@ -8,10 +8,14 @@ const FavouritesPage = () => {
 
 	return (
 		<div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
-			<ul className='list-none'>
+			<ul className='list-none space-y-2'>
 				{favourites.map((f) => (
 					<li key={f}>
-						<a href={f} target='_blank'>{f}</a>
+						<a
+							className='block p-4 text-gray-500 bg-white hover:bg-gray-100 hover:text-gray-700 rounded-lg shadow transition-all duration-300 ease-in-out'
+							href={f}
+							target='_blank'
+						>{f}</a>
 					</li>
 				))}
 			</ul>
